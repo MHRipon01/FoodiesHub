4c894c7882008c9820576c1076758b4b822d74d5
Tanjil Ahamed <tanjil113355@gmail.com>
2024-02-20 22:11:31 +0600

    popular foods component added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
+import PopularFoods from '@/components/home/PopularFoods';
 import Slider from '@/components/home/Slider';
-import Container from '@/components/shared/Container';
 
 export default function Home() {
   return (
     <main>
-     <Slider />
+      <Slider />
+      <PopularFoods />
     </main>
   );
 }
