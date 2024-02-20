21ec6d3e2f4c5c072f51c2d6ffd557dd9fa242f1
Karim Mia <121242355+karim742@users.noreply.github.com>
2024-02-20 18:08:55 +0000

    Upcomming item or restaurant component added

---
+++ src/components/home/Upcomming.js
@@ -0,0 +1,13 @@
+import Container from '../shared/Container';
+
+const Upcomming = () => {
+  return (
+    <div>
+      <Container>
+        <h1>This is a Upcomming component</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default Upcomming;
