94b26664f122b560eab35accee83375e0efd1843
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-02-21 21:21:02 +0600

    Added basic navbar outline

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,10 +1,18 @@
+import Link from 'next/link';
 import Container from './Container';
 
 const Header = () => {
   return (
     <header>
       <Container>
-        <h1>This is header component</h1>
+        <div className='flex h-24 w-full items-center justify-between border-b-[1.5px] py-4'>
+          <div className='text-xl'>FoodiesHub</div>
+          <div></div>
+          <div className='flex gap-4'>
+            <Link href={''}>Login</Link>
+            <Link href={''}>Sign Up</Link>
+          </div>
+        </div>
       </Container>
     </header>
   );
